d50b1839d1dbf3933dabe2b90d0e3cf74edad63c
Jugal1107 <63502751+Jugal1107@users.noreply.github.com>
2021-05-28 21:55:49 +0100

    Mobile Browesr - Appium - New class file

---
+++ src/test/java/com/appium/android/mobile/MobileChromeBrowser_Jugal.java
@@ -0,0 +1,41 @@
+package com.appium.android.mobile;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class MobileChromeBrowser_Jugal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Set the Desired Capabilities
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("deviceName", "70bd90d1");
+		caps.setCapability("platformName", "Android");
+		caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		caps.setCapability(CapabilityType.VERSION, "8.1.0");
+
+		// Instantiate Appium Driver
+		AppiumDriver<MobileElement> driver = null;
+		try {
+			driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+
+		} catch (MalformedURLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		// Open URL in Chrome Browser
+		driver.get("https://www.currys.co.uk");
+	}
+
+}
